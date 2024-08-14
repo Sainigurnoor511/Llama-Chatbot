78f0579ad4c0f91dac694a8a3c59e2d27ad818c4
Sainigurnoor511 <sainigurnoor511@gmail.com>
2024-08-14 16:05:38 +0530

    Title changed 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🥘🍲🍔🍟🍕🥙🍜🍝 Dish Description Generating")
+st.set_page_config(page_title= "🍕Dish Description Generator")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
+    st.title("🍕Dish Description Generator")
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
